9588a410d32254aa8c39a4f6ba71f98260624489
drclcomputers <drclcomputers@gmail.com>
2025-03-19 09:38:28 +0200

    Minor CHanges

--- main.go
+++ main.go
@@ -11,22 +11,50 @@ import (
 	"time"
 )
 
-const K0 = 273.15
-
-func conv_K_C(degrees float64) float64 { //convert K to C
-	d := float64(degrees)
-	return d - K0
+type weather struct {
+	Name  string `json:"name"`
+	Coord struct {
+		LONG float64 `json:"lon"`
+		LAT  float64 `json:"lat"`
+	}
+	Sys struct {
+		Country string  `json:"country"`
+		Sunrise float64 `json:"sunrise"`
+		Sunset  float64 `json:"sunset"`
+	} `json:"sys"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		MaxTemp   float64 `json:"temp_max"`
+		MinTemp   float64 `json:"temp_min"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Weatherdata []struct {
+		Description string `json:"description"`
+	} `json:"weather"`
+	Wind struct {
+		Speed float64 `json:"speed"`
+		Deg   float64 `json:"deg"`
+	} `json:"wind"`
 }
 
-func directwind(deg float64) string {
+const C0 = 273.15 //0°C = 273K
+
+func conv_K_C(degrees float64) float64 { //convert K to C
+	return degrees - C0
+}
+
+func directwind(deg float64) string { //Get wind direction
 	directions := []string{"N", "NE", "E", "SE", "S", "SW", "W", "NW"}
 	index := int((deg+22.5)/45.0) % 8
 	return directions[index]
 }
 
 func main() {
-	fmt.Println("GOWEATHER - ver 0.9.4")
-	apikey := "INSERT YOUR OPENWEATHERMAP API KEY"
+	//Welcome page and city input **********************************************************************
+	fmt.Println("GOWEATHER - ver 0.1.2")
+	apikey := "INSERT YOUR API KEY HERE"
 	fmt.Print("Input city to view forecast: ")
 
 	var city string
@@ -34,7 +62,9 @@ func main() {
 	scanner.Scan()
 	city = scanner.Text()
 	urlcity := url.QueryEscape(city)
+	//**************************************************************************************************
 
+	//Fetching data from API ***************************************************************************
 	fullurl := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?&appid=%s&q=%s", apikey, urlcity)
 
 	resp, err := http.Get(fullurl)
@@ -49,67 +79,50 @@ func main() {
 		fmt.Println("Error fetching data!")
 		return
 	}
+	//**************************************************************************************************
 
-	var data map[string]interface{}
-	json.Unmarshal(body, &data)
+	//Parse JSON to string ******************************
+	var data weather
 
-	if sys, exist := data["sys"].(map[string]interface{}); exist {
-		if country, ok := sys["country"].(string); ok {
-			fmt.Printf("Location: %s, %s\n", city, country)
-		}
-	}
-
-	if weather, exist := data["weather"].([]interface{}); exist && len(weather) > 0 {
-		if description, ok := weather[0].(map[string]interface{})["description"].(string); ok {
-			fmt.Printf("Weather: %s\n", description)
-		}
-	}
-
-	if temp, exist := data["main"].(map[string]interface{})["temp"].(float64); exist {
-		fmt.Printf("Temperature: %.2f°C\n", conv_K_C(temp))
-	} else {
-		fmt.Println("City doesn't exist!")
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Error parsing JSON response.")
 		return
 	}
+	//****************************************************
 
-	if feel, exist := data["main"].(map[string]interface{})["feels_like"].(float64); exist {
-		fmt.Printf("Feels like: %.2f°C\n", conv_K_C(feel))
+	//Print Data *************************************************************************
+	fmt.Printf("Location: %s, %s\n", data.Name, data.Sys.Country)
+
+	if len(data.Weatherdata) > 0 {
+		fmt.Printf("Weather: %s\n", data.Weatherdata[0].Description)
 	}
 
-	if pres, exist := data["main"].(map[string]interface{})["pressure"].(int); exist {
-		fmt.Printf("Pressure: %dhPa -> ", pres)
-		if pres < 980 {
-			fmt.Printf("Low pressure! Cloudy/Raining!")
-		} else if pres > 1000 {
-			fmt.Printf("High pressure! Sunny!")
-		} else {
-			fmt.Printf("Normal pressure!")
-		}
-		fmt.Printf("\n")
-	}
+	fmt.Printf("Temperature: %.0f°C\n", conv_K_C(data.Main.Temp))
 
-	if humid, exist := data["main"].(map[string]interface{})["humidity"].(float64); exist {
-		fmt.Printf("Humidity: %.0f%%\n", humid)
-	}
+	fmt.Printf("Feels like: %.0f°C\n", conv_K_C(data.Main.FeelsLike))
 
-	if wind, exist := data["wind"].(map[string]interface{})["speed"].(float64); exist {
-		fmt.Printf("Wind: %.2fkm/h -> From: ", wind*3.6)
-	}
+	fmt.Printf("Max temperature: %.0f°C\n", conv_K_C(data.Main.MaxTemp))
 
-	if wind, exist := data["wind"].(map[string]interface{}); exist {
-		if windDeg, ok := wind["deg"].(float64); ok {
-			fmt.Printf("%s\n", directwind(windDeg))
-		}
-	}
+	fmt.Printf("Min temperature: %.0f°C\n", conv_K_C(data.Main.MinTemp))
 
-	if sys, exist := data["sys"].(map[string]interface{}); exist {
-		if sunrise, ok := sys["sunrise"].(float64); ok {
-			fmt.Printf("Sunrise: %s\n", time.Unix(int64(sunrise), 0).Format("15:04"))
-		}
-		if sunset, ok := sys["sunset"].(float64); ok {
-			fmt.Printf("Sunset: %s\n", time.Unix(int64(sunset), 0).Format("15:04"))
-		}
+	fmt.Printf("Pressure: %dhPa -> ", data.Main.Pressure)
+	switch {
+	case data.Main.Pressure < 980:
+		fmt.Printf("Low pressure -> Cloudy/Raining!")
+	case data.Main.Pressure > 1000:
+		fmt.Printf("High pressure -> Sunny!")
+	default:
+		fmt.Printf("Normal pressure")
 	}
+	fmt.Printf("\n")
+
+	fmt.Printf("Humidity: %d%%\n", data.Main.Humidity)
+
+	fmt.Printf("Wind: %.1fkm/h -> From: ", data.Wind.Speed*3.6)
+	fmt.Printf("%s\n", directwind(data.Wind.Deg))
+
+	fmt.Printf("Sunrise: %s GMT+2\n", time.Unix(int64(data.Sys.Sunrise), 0).Format("15:04"))
+	fmt.Printf("Sunset: %s GMT+2\n", time.Unix(int64(data.Sys.Sunset), 0).Format("15:04"))
 
 	fmt.Println("\nPress any key to continue...")
 	fmt.Scanln()
